ef7f5246755e7f36c27d44e07747d1c91932042b
Guntbert Reiter <re@htlweiz.at>
2018-06-12 13:06:22 +0200

    Create two (minmal) classes
    Need to be changed, later

---
+++ Fahrtenbuecher/DriversLogBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrtenbuecher
+{
+    /// <summary>
+    /// Ein Fahrtenbuch
+    /// </summary>
+    /// <remarks> VORLÄUFIG public</remarks>
+    public class DriversLogBook
+    {
+        string LicensePlateNumber;
+        List<Entry> Entries;
+    }
+}
---
+++ Fahrtenbuecher/Entry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrtenbuecher
+{
+    /// <summary>
+    /// Eine Zeile im Fahrtenbuch
+    /// </summary>
+    /// <remarks> Dient nur zum Speichern/Zusammenfassen 
+    ///   von Informationen - daher public (nicht gekapselt)
+    /// </remarks>
+    public class Entry
+    {
+        DateTime date;
+        double distance;
+    }
+}
+
+// Encapsulation (Kapselung)
