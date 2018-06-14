4cd9759b30c03ff808308582082817382bf78734
guntbert <re@htlweiz.at>
2018-06-14 20:52:04 +0200

    Show usage of public members

--- Fahrtenbuecher/Program.cs
+++ Fahrtenbuecher/Program.cs
@@ -14,7 +14,22 @@ namespace Fahrtenbuecher
             DriversLogBook johnsLogBook = new DriversLogBook();
 
             billsLogBook.LicensePlateNumber = "WZ 12345A";
+            billsLogBook.Entries = new List<Entry>();
+            Entry newEntry = new Entry();
+            newEntry.date = DateTime.Now.Date;
+            newEntry.distance = 134.5;
+            billsLogBook.Entries.Add(newEntry);
+            WriteLogbook(billsLogBook);
             Console.ReadKey();
+        }
+
+        static void WriteLogbook(DriversLogBook logbook)
+        {
+            Console.WriteLine(logbook.LicensePlateNumber);
+            foreach(Entry entry in logbook.Entries)
+            {
+                Console.WriteLine($"{entry.date:d}: {entry.distance} km");
+            }
         }
     }
 }
